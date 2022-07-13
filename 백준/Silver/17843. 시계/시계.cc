14e3fdefc9c0fa46f0bb593bb74357a9e935e9d3
Yurim Lee <101806773+yurimn@users.noreply.github.com>
2022-07-13 15:26:35 +0900

    [Silver V] Title: 시계, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17843. 시계/시계.cc
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+using namespace std;
+typedef long long ll;
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define rep(i,a,b) for (auto i = (a); i < (b); i++)
+#define each(x, a) for (auto& x: a)
+#define endl '\n'
+
+#define debug if constexpr (!ndebug) cout << "[DEBUG] "
+#define debugv(x) if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugc(c) if constexpr (!ndebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+
+ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b){if(a&&b)return a*(b/gcd(a,b)); return a+b;}
+ll POW(ll a, ll b, ll rem){ll p=1;for(;b;b/=2,a=(a*a)% rem)if(b&1)p=(p*a)%rem;return p;}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    ll i,j;
+    long double t, h, m, s;
+    vector<long double> a(3);
+    cin >> t;
+    while(t--) {
+        cin >> h >> m >> s;
+        h = h*30 + m*0.5 + s*0.5/60.0;
+        m = m*6 + s*0.1;
+        s *= 6;
+        a[0] = abs(h-m);
+        a[1] = abs(m-s);
+        a[2] = abs(s-h);
+        for(auto &k:a) {
+            if(k > 180) k = 360-k;
+        }
+        cout <<fixed<<setprecision(6) <<*min_element(all(a)) << '\n';
+
+    }
+
+
+    return 0;
+}
