a0a5206a252176bd63ecc32566a57200c628eef3
Yurim Lee <101806773+yurimn@users.noreply.github.com>
2022-07-13 17:18:44 +0900

    [Silver IV] Title: Speed Typing, Time: 284 ms, Memory: 2684 KB -BaekjoonHub

---
+++ 백준/Silver/24839. Speed Typing/Speed Typing.cc
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+using namespace std;
+typedef long long ll;
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define rep(i,a,b) for (auto i = (a); i < (b); i++)
+#define each(x, a) for (auto& x: a)
+#define endl '\n'
+
+#define debug if constexpr (!ndebug) cout << "[DEBUG] "
+#define debugv(x) if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugc(c) if constexpr (!ndebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+
+ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b){if(a&&b)return a*(b/gcd(a,b)); return a+b;}
+ll POW(ll a, ll b, ll rem){ll p=1;for(;b;b/=2,a=(a*a)% rem)if(b&1)p=(p*a)%rem;return p;}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    ll i,j;
+    ll n;
+    cin >> n;
+    for(i = 1; i <= n; i++) {
+        string I, P;
+        cin >> I >> P;
+        cout << "Case #" << i << ": ";
+        ll num = 0;
+
+        for(j = 0; j < I.size(); j++) {
+            if(find(P.begin() + j, P.end(), I[j]) == P.end()){
+                cout << "IMPOSSIBLE" << '\n';
+                break;
+            }
+            num += find(P.begin() + j, P.end(), I[j])-P.begin() - j;
+            P.erase(P.begin()+j, find(P.begin() + j, P.end(), I[j]));
+        }
+        if(j == I.size()) cout << num + P.size()-I.size() << '\n';
+    }
+
+    return 0;
+}
