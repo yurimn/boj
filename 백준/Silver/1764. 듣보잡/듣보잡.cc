4c732dec4cc3553cdf6c6352be4fbdb6c7601c85
Yurim Lee <101806773+yurimn@users.noreply.github.com>
2022-07-14 15:23:04 +0900

    [Silver IV] Title: 듣보잡, Time: 40 ms, Memory: 9912 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+using namespace std;
+typedef long long ll;
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define rep(i,a,b) for (auto i = (a); i < (b); i++)
+#define each(x, a) for (auto& x: a)
+#define endl '\n'
+
+#define debug if constexpr (!ndebug) cout << "[DEBUG] "
+#define debugv(x) if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugc(c) if constexpr (!ndebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+
+ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b){if(a&&b)return a*(b/gcd(a,b)); return a+b;}
+ll POW(ll a, ll b, ll rem){ll p=1;for(;b;b/=2,a=(a*a)% rem)if(b&1)p=(p*a)%rem;return p;}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    ll i,j;
+    ll n, m;
+    cin >> n>>m;
+    vector<string> per(n);
+    vector<string> per2(m);
+    vector<string> re;
+    ll cnt = 0;
+    string st;
+    for(auto &k:per) cin >> k;
+    for(auto &k:per2) cin >> k;
+    sort(all(per));
+    sort(all(per2));
+    for(auto&k:per){
+        if(binary_search(all(per2),k))re.pb(k);
+    }
+    cout << re.size() <<'\n';
+    for(auto k:re) cout << k << '\n';
+    return 0;
+}
