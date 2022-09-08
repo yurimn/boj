0c145bda5aaee4645497c2e08c00a9e3f8dd20e6
Yurim Lee <101806773+yurimn@users.noreply.github.com>
2022-09-08 12:13:28 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2652 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+using namespace std;
+typedef long long ll;
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define rep(i,a,b) for (auto i = (a); i < (b); i++)
+#define each(x, a) for (auto& x: a)
+#define endl '\n'
+
+#define debug if constexpr (!ndebug) cout << "[DEBUG] "
+#define debugv(x) if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugc(c) if constexpr (!ndebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+
+ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b){if(a&&b)return a*(b/gcd(a,b)); return a+b;}
+ll POW(ll a, ll b, ll rem){ll p=1;for(;b;b/=2,a=(a*a)% rem)if(b&1)p=(p*a)%rem;return p;}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    ll i,j,n,k;
+    cin>>n;
+    vector<vector<ll>> v(n, vector<ll> (3));
+    cin >> v[0][1];
+    for(i=1;i<n;i++){
+        cin >> k;
+        v[i][0] = max({v[i-1][0], v[i-1][1], v[i-1][2]});
+        v[i][1] = v[i-1][0] + k;
+        v[i][2] = v[i-1][1] + k;
+    }
+    cout << max({v[n-1][0], v[n-1][1], v[n-1][2]});
+
+    return 0;
+}
